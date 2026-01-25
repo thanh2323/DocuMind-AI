b556a3689d89cea9caf281f18f53bd42bbd3dfe7
thanh2323 <nguyenchithanh2213@gmail.com>
2026-01-25 17:37:53 +0700

    fix: update CORS allowed origins and enable withCredentials for API requests

--- FE/src/services/api.js
+++ FE/src/services/api.js
@@ -5,6 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
